c88ab52f4c605bcbf4d3b75c4cd4e2ae8a280074
varun-Tad <varuntad141@gmail.com>
2025-02-14 09:17:24 -0500

    code reactoring and adding additional components

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let sideBar = document.getElementById("sidebar");
 let menuBtn = document.querySelector(".menu-btn");
-let closeBtn = document.querySelector(".close-btn");
+let closeBtn = document.querySelector(".closeBtn");
 
 menuBtn.addEventListener("click", () => {
   sideBar.style.left = "0";
@@ -8,4 +8,11 @@ menuBtn.addEventListener("click", () => {
 
 closeBtn.addEventListener("click", () => {
   sideBar.style.left = "-100%";
+});
+
+let tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+let tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
 });
