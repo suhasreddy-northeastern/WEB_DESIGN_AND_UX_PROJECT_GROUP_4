8e4c0c90136730c1c21ab812f9496df7b9191833
varun-Tad <varuntad141@gmail.com>
2025-02-13 22:39:35 -0500

    homepage code refactoring navbar logo addition

--- script.js
+++ script.js
@@ -3,16 +3,9 @@ let menuBtn = document.querySelector(".menu-btn");
 let closeBtn = document.querySelector(".close-btn");
 
 menuBtn.addEventListener("click", () => {
-  console.log("menu ");
   sideBar.style.left = "0";
 });
 
 closeBtn.addEventListener("click", () => {
   sideBar.style.left = "-100%";
 });
-
-// function toggleAccordion(collapseId) {
-//   const collapseElement = new bootstrap.Collapse(`#${collapseId}`, {
-//     toggle: true, // Toggles the collapse state (open/close)
-//   });
-// }
